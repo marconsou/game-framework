3427dbf9709d4922a6bbe52557b93cf0a8a68290
Marcelo Contarini de Souza <marcelocontarinisouza@gmail.com>
2024-05-25 21:13:58 -0300

    WindowsCompact

---
+++ Game Framework Library/Source/App/Windows/WindowsCompact.h
@@ -0,0 +1,23 @@
+#ifndef WINDOWSCOMPACT_H
+#define WINDOWSCOMPACT_H
+
+#include <winsdkver.h>
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0A00
+#endif
+#include <sdkddkver.h>
+
+#define NOMINMAX
+#define NODRAWTEXT
+#define NOGDI
+#define NOBITMAP
+#define NOMCX
+#define NOSERVICE
+#define NOHELP
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+#include <wrl/client.h>
+
+#endif
