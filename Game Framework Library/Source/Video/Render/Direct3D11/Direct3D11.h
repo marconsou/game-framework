c720bb860ed9a2e8b5a88fcd3fc5bc25d9872f5f
Marcelo Contarini de Souza <marcelocontarinisouza@gmail.com>
2024-05-26 03:26:01 -0300

    Direct3D11

---
+++ Game Framework Library/Source/Video/Render/Direct3D11/Direct3D11.h
@@ -0,0 +1,14 @@
+#ifndef DIRECT3D11_H
+#define DIRECT3D11_H
+
+#include <d3d11_4.h>
+#include <dxgi1_6.h>
+
+#include <DirectXMath.h>
+#include <DirectXColors.h>
+
+#ifdef _DEBUG
+#include <dxgidebug.h>
+#endif
+
+#endif
