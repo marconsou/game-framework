09567126039a139b62c6f9ee1595c9a0e830371b
Marcelo Contarini de Souza <marcelocontarinisouza@gmail.com>
2024-05-27 12:15:36 -0300

    DirectXTK Input header

---
+++ Game Framework Library/Source/Input/Windows/DirectXTKKeyboard.h
@@ -0,0 +1,7 @@
+#ifndef DIRECTXTKKEYBOARD_H
+#define DIRECTXTKKEYBOARD_H
+
+#include "../../App/Windows/WindowsCompact.h"
+#include <directxtk/Keyboard.h>
+
+#endif
---
+++ Game Framework Library/Source/Input/Windows/DirectXTKMouse.h
@@ -0,0 +1,7 @@
+#ifndef DIRECTXTKMOUSE_H
+#define DIRECTXTKMOUSE_H
+
+#include "../../App/Windows/WindowsCompact.h"
+#include <directxtk/Mouse.h>
+
+#endif
